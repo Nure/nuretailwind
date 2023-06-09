af5e69ae0976d6af8ffe333ead7d5ad8ca5ba14b
Nure <nalamsiddiq@gmail.com>
2023-06-09 14:47:41 +0600

    Add Tailwind Project

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'dark': '#000614',
+        'text': 'white',
+        'primary': '#E50914',
+        'secondary': 'red'
+      },
+      
+    },
+  },
+  plugins: [],
+}
+
